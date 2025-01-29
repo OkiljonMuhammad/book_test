f86063b6422a0088d4f4603dd9b6e6b2e7834af5
OkiljonMuhammad <maxmadmurodovoqiljon@gmail.com>
2025-01-29 14:03:39 +0500

    port changed

--- server.js
+++ server.js
@@ -69,5 +69,5 @@ app.get("/books", (req, res) => {
     res.json({ books });
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
